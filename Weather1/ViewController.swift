7d251102ec07b06ac6a21e612b475368e5045cdd
thibaudg1 <96113614+thibaudg1@users.noreply.github.com>
2023-03-22 11:42:02 +0200

    FEAT: Add first UI elements

--- Weather1/ViewController.swift
+++ Weather1/ViewController.swift
@@ -7,6 +7,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var descriptionLabel: UILabel!
+    @IBOutlet var background: UIImageView!
+    @IBOutlet var city: UILabel!
+    @IBOutlet var icon: UIImageView!
+    @IBOutlet var temperature: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
