d6cf9612af248855122cab02304ad9b952764d41
thibaudg1 <96113614+thibaudg1@users.noreply.github.com>
2023-03-23 16:28:22 +0200

    FEAT: Add querying geocoding from Weather service

---
+++ Weather1/City.swift
@@ -0,0 +1,32 @@
+//
+//  City.swift
+//  Weather1
+//
+
+import Foundation
+
+struct City: Decodable {
+    let name: String
+    let localizedNames: Dictionary<String, String>?
+    let country: String
+    let state: String?
+    let latitude: Double
+    let longitude: Double
+    
+    enum CodingKeys: String, CodingKey {
+        case name, state, country
+        case localizedNames = "local_names"
+        case latitude = "lat"
+        case longitude = "lon"
+    }
+}
+
+extension City {
+    static let cities: [City] = [
+        City(name: "London", localizedNames: [:], country: "GB", state: nil, latitude: 51.5085, longitude: -0.1257),
+        City(name: "London", localizedNames: [:], country: "CA", state: nil, latitude: 42.9834, longitude: -81.233),
+        City(name: "London", localizedNames: [:], country: "US", state: "OH", latitude: 39.8865, longitude: -83.4483),
+        City(name: "London", localizedNames: [:], country: "US", state: "KY", latitude: 37.129, longitude: -84.0833),
+        City(name: "London", localizedNames: [:], country: "US", state: "CA", latitude: 36.4761, longitude: -119.4432)
+    ]
+}
--- Weather1/OpenWeatherAPI/APIService.swift
+++ Weather1/OpenWeatherAPI/APIService.swift
@@ -9,10 +9,13 @@ typealias Coordinate = (latitude: Double, longitude: Double)
 typealias WeatherForecastCompletionBlock = (Result<CurrentWeatherResponse, APIServiceError>) -> Void
 typealias ImageData = Data
 typealias IconCompletionBlock = (Result<ImageData, APIServiceError>) -> Void
+typealias CityResultsCompletionBlock = (Result<[City], APIServiceError>) -> Void
 
 protocol APIService {
     func fetchCurrentWeather(coordinate: Coordinate,
                              completionHandler: @escaping WeatherForecastCompletionBlock)
+    func fetchGeocodedCities(search: String,
+                             completionHandler: @escaping CityResultsCompletionBlock)
 }
 
 enum APIServiceError: Error {
@@ -73,6 +76,39 @@ final class OpenWeatherAPIService: APIService {
         }.resume()
     }
     
+    func fetchGeocodedCities(search: String,
+                             completionHandler: @escaping CityResultsCompletionBlock) {
+        guard let url = cityResultsUrlFor(search) else {
+            completionHandler(.failure(.invalidURL))
+            return
+        }
+        
+#if DEBUG
+        print(url)
+#endif
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data,
+                  let response = response as? HTTPURLResponse,
+                  error == nil else {
+                completionHandler(.failure(.emptyDataOrError))
+                return
+            }
+            
+            guard 200 ..< 300 ~= response.statusCode else {
+                completionHandler(.failure(.unexpectedStatusCode(response.statusCode)))
+                return
+            }
+            
+            do {
+                let cities = try JSONDecoder().decode([City].self, from: data)
+                completionHandler(.success(cities))
+            } catch {
+                completionHandler(.failure(.unableToParseDataWith(error: error)))
+            }
+        }.resume()
+    }
+    
     func fecthWeatherIcon(named iconName: String, completionHandler: @escaping IconCompletionBlock) {
         guard let url = iconUrlFor(iconName) else {
             completionHandler(.failure(.invalidURL))
@@ -116,11 +152,7 @@ private extension OpenWeatherAPIService {
             .init(name: "lang", value: "en")
         ]
         
-        guard let completeUrl = components?.url else {
-            return nil
-        }
-        
-        return completeUrl
+        return components?.url
     }
     
     func iconUrlFor(_ icon: String) -> URL? {
@@ -128,5 +160,20 @@ private extension OpenWeatherAPIService {
                     .appending(icon)
                     .appending("@2x.png")        
         return URL(string: str)
+    }
+    
+    func cityResultsUrlFor(_ city: String) -> URL? {
+        guard let url = URL(string: "https://api.openweathermap.org/geo/1.0/direct") else {
+            return nil
+        }
+        
+        var components = URLComponents(url: url, resolvingAgainstBaseURL: true)
+        components?.queryItems = [
+            .init(name: "q", value: city),
+            .init(name: "limit", value: "1"),
+            .init(name: "appid", value: apiKey)
+        ]
+        
+        return components?.url
     }
 }
